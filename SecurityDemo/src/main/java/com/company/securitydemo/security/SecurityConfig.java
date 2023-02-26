a547e72680a0e05f9675f9eb3228e8392916ce3c
rinat18 <haciyev.rinat.2002@gmail.com>
2023-02-26 17:42:53 +0400

    Database Authentication

--- SecurityDemo/src/main/java/com/company/securitydemo/security/SecurityConfig.java
+++ SecurityDemo/src/main/java/com/company/securitydemo/security/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.company.securitydemo.security;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
-import org.springframework.http.*;
+import org.springframework.security.authentication.dao.*;
 import org.springframework.security.config.annotation.method.configuration.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
@@ -10,8 +10,10 @@ import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.password.*;
 import org.springframework.security.provisioning.*;
 import org.springframework.security.web.*;
+import org.springframework.security.web.util.matcher.*;
 
-import static com.company.securitydemo.security.UserPermission.COURSE_WRITE;
+import java.util.concurrent.*;
+
 import static com.company.securitydemo.security.UserRole.*;
 
 @Configuration
@@ -20,58 +22,59 @@ import static com.company.securitydemo.security.UserRole.*;
 public class SecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
 
     @Autowired
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
+    public SecurityConfig(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
         this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
     }
 
-    @Bean
-    protected UserDetailsService userDetailsService(){
-        UserDetails rinatUser = User.builder()
-                .username("rinat")
-                .password(passwordEncoder.encode("password"))
-//                .roles(STUDENT.name()) //ROLE_STUDENT
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
+   @Bean
+   public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userDetailsService);
 
-        UserDetails alexUser = User.builder()
-                .username("Alex")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMIN.name())
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails tomUser = User.builder()
-                .username("tom")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(UserRole.ADMIN_TRAINEE.name())
-                .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(
-                rinatUser,
-                alexUser,
-                tomUser
-        );
-    }
+        return provider;
+   }
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
-       return http
-//                .csrf (csrf->csrf.disable())
+        return http
+                .csrf (csrf->csrf.disable())
                 .authorizeRequests(
                         auth -> {
                             auth.antMatchers("/", "css/*", "js/*", "index").permitAll();
                             auth.antMatchers("/api/**").hasRole(STUDENT.name());
-//                            auth.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission());
-//                            auth.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission());
-//                            auth.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission());
-//                            auth.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name());
                             auth.anyRequest().authenticated();
-                })
-                .httpBasic()
-                .and().build();
+                        })
+                .formLogin(
+                        form ->{
+                            form.loginPage("/login").permitAll();
+                            form.defaultSuccessUrl("/courses");
+                            form.usernameParameter("username");
+                            form.passwordParameter("password");
+                        }
+                )
+                .rememberMe(
+                        remember ->{
+                            remember.tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(15));
+                            remember.key("keyToHashTheContent");
+                            remember.rememberMeParameter("remember-me");
+                        }
+                )
+                .logout(
+                        logout->{
+                            logout.logoutUrl("/logout");
+                            logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"));
+                            logout.clearAuthentication(true);
+                            logout.invalidateHttpSession(true);
+                            logout.deleteCookies("JSESSIONID", "remember-me");
+                            logout.logoutSuccessUrl("/login");
+                        }
+                )
+                .build();
     }
 
 }
